3fddb7bd3ef7e658f3dee8c5ba6acac59b17790e
win7 <737141437@qq.com>
2018-10-09 10:00:14 +0800

    1、没有完成的Demo

--- SpringBootWebservice/src/main/java/com/example/demo/conf/WSClientConfig.java
+++ SpringBootWebservice/src/main/java/com/example/demo/conf/WSClientConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
+
 /**
  * webservice 客户端配置
  * 
